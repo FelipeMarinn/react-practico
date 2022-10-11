78d0a80a1b876dff8198db9f0e89e93d1a8514c5
Daniel <felipeguisao18@gmail.com>
2022-10-11 15:41:52 -0500

    [UPDATE] App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
 	return (
 		<AppContext.Provider value={ initialState }>
-			<BrowserRouter basename='/reactPractico'>
+			<BrowserRouter>
 			  <Layout>
 			  	  <Switch>
 			  	  	  <Route exact path="/" component={Home} />
